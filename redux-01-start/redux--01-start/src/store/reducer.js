d333f39d9ac26a78c8c52f2e3c53544332fb0d9d
shubham singh <shubhamit3038@gmail.com>
2019-03-09 10:32:36 +0530

    using switch case in reducer

--- redux-01-start/redux--01-start/src/store/reducer.js
+++ redux-01-start/redux--01-start/src/store/reducer.js
@@ -3,26 +3,25 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    if(action.type === "INCREMENT" ) {
-        return {
-            counter: state.counter + 1
-        };
+    switch(action.type){
+       case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'ADDITION':
+            return {
+                counter: state.counter + action.value
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'SUBTRACTION':
+            return {
+                counter: state.counter - action.value
+            }
     }
-    if(action.type === "ADDITION" ) {
-        return {
-            counter: state.counter + action.value
-        };
-    }
-    if(action.type === "DECREMENT" ) {
-        return {
-            counter: state.counter - 1
-        };
-    }
-    if(action.type === "SUBTRACTION" ) {
-        return {
-            counter: state.counter - action.value
-        };
-    }
+   
     return state;
 };
 
